1900a7b99bce7544852cf9638772f56cfb20271b
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-12 20:03:50 +0545

    Added:All images related to admin

--- obj/Debug/net9.0/LMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/LMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccb604fdfd5c9febdef6f0f4563a7f8211ce9d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85362a180af05ae010dec7ee5e64cf1ac2d92615")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
